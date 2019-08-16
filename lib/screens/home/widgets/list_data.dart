aaaf76a5fab8fad839e08a5593551c4548ebaebc
jrguedes <jrguedes.ja@gmail.com>
2019-08-16 09:49:00 -0300

    Refactoring finished.

--- lib/screens/home/widgets/list_data.dart
+++ lib/screens/home/widgets/list_data.dart
@@ -14,47 +14,59 @@ class ListData extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return buildTaskContainer();
+  }
+
+  Container buildTaskContainer() {
     return Container(
       margin: margin,
       decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border(
-          top: BorderSide(color: Colors.blueGrey[100], width: 1),
-          bottom: BorderSide(color: Colors.blueGrey[100], width: 1),
-        )
-      ),
+          color: Colors.white,
+          border: Border(
+            top: BorderSide(color: Colors.blueGrey[100], width: 1),
+            bottom: BorderSide(color: Colors.blueGrey[100], width: 1),
+          )),
       child: Row(
         children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(top: 10, bottom: 10, left: 8, right:10),
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(image: image),
-
-            ),
-          ),
+          buildCircularProfileContainer(),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text(
-                title, style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w400
+              buildTitleText(),
+              SizedBox(
+                height: 5,
               ),
-              ),
-              SizedBox(height: 5,),
-              Text(
-                subtitle, style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.w300
-              ),
-              )
+              buildSubtitleText()
             ],
           )
         ],
+      ),
+    );
+  }
+
+  Text buildSubtitleText() {
+    return Text(
+      subtitle,
+      style: TextStyle(
+          fontSize: 18, color: Colors.grey, fontWeight: FontWeight.w300),
+    );
+  }
+
+  Text buildTitleText() {
+    return Text(
+      title,
+      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+    );
+  }
+
+  Container buildCircularProfileContainer() {
+    return Container(
+      margin: EdgeInsets.only(top: 10, bottom: 10, left: 8, right: 10),
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(image: image),
       ),
     );
   }
